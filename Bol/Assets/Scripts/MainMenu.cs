11644aea32c3b067a054959a6e38c73520ac5ee2
ArtixBot <williamwongcastle@gmail.com>
2018-02-24 14:34:50 -0500

    dasdf
    f

---
+++ Bol/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button creditButton;
+	public Button exitButton;
+
+	void Awake()
+	{
+		creditButton.onClick.AddListener(() => OnClickCredit());
+		exitButton.onClick.AddListener(() => OnClickExit());
+	}
+
+	void OnClickCredit()
+	{
+		SceneManager.LoadScene("CreditsMenu");
+	}
+
+	void OnClickExit(){
+		Application.Quit ();
+	}
+}
