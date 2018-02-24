0a627f7f13851826dcd93e809b2b3d8d64b8f063
Jeffrey-Wang <17jwapples@gmail.com>
2018-02-24 14:43:26 -0500

    New EndOfLevelTriggerBody prefab

---
+++ Bol/Assets/Scripts/EndOfLevelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			print ("Info: End of level trigger");
+			//implement level transition here
+			//Destroy (this.gameObject);
+		}
+	}
+}
