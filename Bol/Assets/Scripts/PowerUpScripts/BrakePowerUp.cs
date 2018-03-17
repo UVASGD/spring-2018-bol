48ba974426c5001cc893e3ded9fe95b0d12ed374
ArtixBot <williamwongcastle@gmail.com>
2018-03-17 14:24:27 -0400

    Brake poweriup framework added

---
+++ Bol/Assets/Scripts/PowerUpScripts/BrakePowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrakePowerUp : PowerUp {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("PowerUpEffect activated or some shit");
+	}
+
+	public BrakePowerUp(){
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void PowerUpEffect(){
+		Debug.Log ("PowerUpEffect activated or some shit");
+	}
+}
