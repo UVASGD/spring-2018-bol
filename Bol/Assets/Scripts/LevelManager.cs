34b3ffb85e411b7c2694ce3100c93180e36a7818
ArtixBot <williamwongcastle@gmail.com>
2018-03-30 20:10:49 -0400

    level manager is up, and main menu now accounts for new camera controls

---
+++ Bol/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+	public Button levelOne;
+
+	// Use this for initialization
+	void Awake () {
+		levelOne.onClick.AddListener (() => GoLevelOne ());
+	}
+
+	void GoLevelOne(){
+		SceneManager.LoadScene("JaredTesting");
+	}
+
+}
