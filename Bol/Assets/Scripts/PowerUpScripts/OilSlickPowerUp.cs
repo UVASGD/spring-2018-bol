17c85f5e84529764d9a412863e7bf1345830c0dc
Andrew Murphy <afm9yy@virginia.edu>
2018-03-31 16:17:30 -0400

    add oilslick powerup

---
+++ Bol/Assets/Scripts/PowerUpScripts/OilSlickPowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilSlickPowerUp : PowerUp {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	
+	public override void PowerUpEffect() {
+
+		GameObject oilSlickPrefab = Resources.Load ("OilSlick") as GameObject;
+		Object.Instantiate (oilSlickPrefab, player.transform.position + new Vector3 (0.0f, 2.0f, 0.0f), Quaternion.identity);
+		
+	}
+
+}
