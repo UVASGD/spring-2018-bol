1368dae7170d4ff1f1743bfaf12dd5f1383f83dc
ArtixBot <williamwongcastle@gmail.com>
2018-03-30 21:30:56 -0400

    WE HAVE A BOMB
    fbi pls no arrest

---
+++ Bol/Assets/Scripts/PowerUpScripts/BombExplosion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Applies an explosion force to all nearby rigidbodies
+public class BombExplosion : MonoBehaviour
+{
+	public float radius = 65.0F;
+	public float power = 2000.0F;
+
+	IEnumerator Start()
+	{
+		yield return new WaitForSeconds (2);
+
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+		foreach (Collider hit in colliders)
+		{
+			Rigidbody rb = hit.gameObject.GetComponent<Rigidbody>();
+
+			if (rb != null)
+				rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+		}
+
+		Destroy(gameObject);
+	}
+
+}
