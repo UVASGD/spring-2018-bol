1c7d3e2620075610df82dbda2b2f6e0b58f37c3e
Jared Capelle <jjc2bd@virginia.edu>
2018-03-30 23:27:25 -0400

    Added some UI to indicate powerup (may need tweaking)

---
+++ Bol/Assets/UIUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIUpdater : MonoBehaviour {
+
+    public Text powerUpText;
+
+	// Use this for initialization
+	void Start () {
+        powerUpText.text = "No PowerUp";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void UpdatePowerUpText(PowerUp powerUp = null)
+    {
+        if (powerUp == null)
+        {
+            powerUpText.text = "No PowerUp";
+        }
+        else
+        {
+            powerUpText.text = "" + powerUp.GetType();
+        }
+    }
+}
