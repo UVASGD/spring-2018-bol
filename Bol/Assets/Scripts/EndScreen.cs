b406c0ff618f06f0f52f78f9cd7a75aa9adc91d1
ArtixBot <williamwongcastle@gmail.com>
2018-04-07 15:44:20 -0400

    Basic Ending Screen

---
+++ Bol/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour {
+
+	public Button mainButton;
+
+	void Awake()
+	{
+		mainButton.onClick.AddListener (() => OnClickMain ());
+	}
+
+	void OnClickMain()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
