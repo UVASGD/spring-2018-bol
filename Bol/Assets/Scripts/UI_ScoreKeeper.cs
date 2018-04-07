50172dcd2d97753a701d40614659ddf5504fbb0d
ArtixBot <williamwongcastle@gmail.com>
2018-04-07 15:23:41 -0400

    Score keeper

---
+++ Bol/Assets/Scripts/UI_ScoreKeeper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ScoreKeeper : MonoBehaviour {
+
+	public Text scoreText;
+	public TurnManager turn;
+
+	// Use this for initialization
+	void Start () {
+		scoreText.text = "0";
+		if (!turn) {
+			turn = (TurnManager)FindObjectOfType(typeof(TurnManager));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = turn.GetCurrentPlayerPoints ().PointTotal.ToString();
+	}
+}
