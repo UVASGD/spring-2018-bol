4f553f7ae1480b87244111208eec537134e2d7b4
Jared Capelle <jjc2bd@virginia.edu>
2018-04-21 16:45:00 -0400

    I think the indicator is working... Needs more testing...

--- Bol/Assets/Scripts/Indicator.cs
+++ Bol/Assets/Scripts/Indicator.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Indicator : MonoBehaviour {
 
-	public GameObject indicatorObj;
-
+    public LineRenderer lr;
 	public PlayerInput curInput;
+    public Rigidbody rb;
 
 	public float minScale = 0.25f;
 	public float maxScale = 1.0f;
+    float oldHorizAngle;
+    float oldVertAngle;
+
+    public int resolution;
+    float velocity;
+    float radAngle;
+    float g;
 
 	// Use this for initialization
 	void Start () {
 		if (!curInput) {
 			curInput = GetComponent<PlayerInput>();
 		}
+        if (!lr)
+        {
+            lr = GetComponent<LineRenderer>();
+        }
+        if (!rb)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        oldHorizAngle = curInput.horizontalAngle;
+        oldVertAngle = curInput.verticalAngle;
 	}
 
 	Vector3 calculateDirectionVector() {
@@ -29,16 +46,49 @@ public class Indicator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 indicatorScale = indicatorObj.transform.localScale;
-		Vector3 newScale = new Vector3(indicatorObj.transform.localScale.x, 
-			Mathf.Lerp(minScale, maxScale, curInput.curPower/curInput.maxPower), 
-			indicatorObj.transform.localScale.z);
-		indicatorObj.transform.localScale = newScale;
-		indicatorObj.transform.position = gameObject.transform.position + calculateDirectionVector().normalized*indicatorObj.transform.localScale.y;
-		indicatorObj.transform.rotation = Quaternion.LookRotation(calculateDirectionVector()*Mathf.Rad2Deg) * Quaternion.Euler(90.0f, 0.0f, 0.0f);
-	}
+        if (AnglesChanged())
+        {
+            g = Physics.gravity.magnitude;
+            float acceleration = curInput.maxPower / rb.mass;
+            velocity = (acceleration * Time.fixedDeltaTime);
+            lr.positionCount = resolution + 1;
+            lr.SetPositions(CalculateArray());
+        }
+    }
+
+    bool AnglesChanged()
+    {
+        bool result = (oldHorizAngle != curInput.horizontalAngle) || (oldVertAngle != curInput.verticalAngle);
+        if (result)
+        {
+            oldVertAngle = curInput.verticalAngle;
+            oldHorizAngle = curInput.horizontalAngle;
+        }
+        return result;
+    }
+
+    Vector3[] CalculateArray()
+    {
+        Vector3[] arcArray = new Vector3[resolution + 1];
+        radAngle = curInput.verticalAngle * Mathf.Deg2Rad;
+        float maxDistance = (velocity * velocity * Mathf.Sin(2 * radAngle));
+        Quaternion rotation = Quaternion.AngleAxis(curInput.horizontalAngle - 90, Vector3.up); // Not sure why it needs the -90, but that's what made it work...
+        for(int i = 0; i <= resolution; i++)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = rotation * CalculateArcPoint(t, maxDistance);
+        }
+        return arcArray;
+    }
+
+    Vector3 CalculateArcPoint(float t, float maxDistance)
+    {
+        float x = t * maxDistance;
+        float y = x * Mathf.Tan(radAngle) - ((g * x * x) / (2 * velocity * velocity * Mathf.Cos(radAngle) * Mathf.Cos(radAngle)));
+        return new Vector3(x,y);
+    }
 
 	public void toggleActive() {
-		indicatorObj.SetActive(false);
+        lr.enabled = !lr.enabled;
 	}
 }
