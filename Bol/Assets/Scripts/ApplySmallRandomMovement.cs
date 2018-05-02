fbdde043605587eb7cd52ea9f24245b4a6bdc178
Jared Capelle <jjc2bd@virginia.edu>
2018-05-02 02:46:31 -0400

    Spruced Up my level a bit. Good Night Everyone

---
+++ Bol/Assets/Scripts/ApplySmallRandomMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplySmallRandomMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public float amount = 0.5f;
+	void Start () {
+        gameObject.transform.Translate(new Vector3(Random.Range(-amount, amount), Random.Range(-amount, amount), Random.Range(-amount, amount)));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
